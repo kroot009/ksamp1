9abc07641806c6a189e9c911e6570eb871a6f3c3
kroot009 <67678421+kroot009@users.noreply.github.com>
2021-02-25 13:48:17 +0900

    20210225_0

--- app/src/main/java/com/example/ksamp1/MainActivity.java
+++ app/src/main/java/com/example/ksamp1/MainActivity.java
@@ -2,83 +2,124 @@ package com.example.ksamp1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-//import android.os.Handler;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.ServerSocket;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.Buffer;
 
 public class MainActivity extends AppCompatActivity {
-    // TextView textView;
-    // Handler handler = new Handler();
-    public InputStream dataIs;
-    public OutputStream dataOs;
+    private Handler mHandler;
+    InetAddress serverAddr;
+    Socket socket;
+    PrintWriter sendWriter;
+    private String ip = "192.168.219.117";
+    // private String ip = "122.34.51.3";
+    private int port = 3337;
+
+    TextView textView;
+    String UserID;
+    Button connectbutton;
+    Button chatbutton;
+    TextView chatView;
+    EditText message;
+    String sendmsg;
+    String read;
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            sendWriter.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mHandler = new Handler();
+        textView = (TextView) findViewById(R.id.textView);
+        chatView = (TextView) findViewById(R.id.chatView);
+        message = (EditText) findViewById(R.id.message);
 
-        Button button = (Button) findViewById(R.id.btn1);
-        //textView = (TextView)findViewById(R.id.textView);
+        //Intent intent = getIntent();
+        Intent intent = new Intent(MainActivity.this, EnterActivity.class);
+        // startActivity(intent);
+        startActivityForResult(intent, 101);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        UserID = intent.getStringExtra("username");
+        textView.setText(UserID);
+        chatbutton = (Button) findViewById(R.id.chatbutton);
+
+        new Thread() {
+            public void run() {
+                try {
+                    InetAddress serverAddr = InetAddress.getByName(ip);
+                    socket = new Socket(serverAddr, port);
+                    sendWriter = new PrintWriter(socket.getOutputStream());
+                    BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    while(true){
+                        read = input.readLine();
+
+                        System.out.println("TTTTTTTT"+read);
+                        if(read!=null){
+                            mHandler.post(new msgUpdate(read));
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } }}.start();
+
+        chatbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ClientThread thread = new ClientThread();
-                thread.start();
+                sendmsg = message.getText().toString();
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        try {
+                            sendWriter.println(UserID +">"+ sendmsg);
+                            sendWriter.flush();
+                            message.setText("");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
             }
         });
     }
 
-    class ClientThread extends Thread {
+    class msgUpdate implements Runnable{
+        private String msg;
+        public msgUpdate(String str) {this.msg=str;}
+
         @Override
         public void run() {
-            int port = 3337;
-            String host = "122.34.51.3";
-            //int port = 3337;
-            //String host = "192.168.0.1";
-            try {
-                Socket socket = new Socket(host, port);
-                //ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
-                //outStream.writeObject("kroothii");
-                //outStream.flush();
-                dataOs = socket.getOutputStream();
-                dataIs = socket.getInputStream();
-
-                //byte[] buff = new byte[1024] = "1234";
-                int rtn;
-                byte[] buff = "1234".getBytes();
-
-                 dataOs.write(buff);
-
-                Log.d("ClientStream", "Send to Server OK");
-
-                //ObjectInputStream instream = new ObjectInputStream(socket.getInputStream());
-                //Object input = (String) instream.readObject();
-                rtn = dataIs.read(buff);
-                String tmp = new String(buff, 0, rtn);
-                Log.d("ClientThread", "Recv: " + tmp);
-
-                //final Object input = (String) instream.readObject();
-                //handler.post(new Runnable() {
-                //    @Override
-                //    public void run() {
-                 //       textView.setText("Recv: " + input);
-                 //   }
-                // });
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            chatView.setText(chatView.getText().toString()+msg+"\n");
         }
     }
 }
